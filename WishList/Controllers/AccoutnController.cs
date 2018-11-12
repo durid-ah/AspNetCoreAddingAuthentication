137fb35633b1e280d67d429fdc33ae731396156b
Durid Ahmad <dahmad@byui.edu>
2018-11-12 15:04:17 -0700

    dependency injection

---
+++ WishList/Controllers/AccoutnController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    [Authorize]
+    public class AccoutnController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccoutnController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+    }
+}
